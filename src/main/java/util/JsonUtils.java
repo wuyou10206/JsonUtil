938cec03ed34a2a965a8891440537e004b106fc1
zhaowei <zw1020688@163.com>
2017-07-20 18:45:43 +0800

    1.对方法增加了注释 2.修复了JSONArray比较是否相等时的一个bug 3.增加了对JSONObject排序的方法 4.增加了对JSONArray排序的方法

--- src/main/java/util/JsonUtils.java
+++ src/main/java/util/JsonUtils.java
@@ -7,10 +7,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by zhaowei on 2017/6/5.
@@ -23,6 +20,12 @@ public class JsonUtils {
         PropertyConfigurator.configure("log4j.properties");
     }
 
+    /**
+     * 判断两个JSONArray是否相等,默认对两个JSONArray按照字符串进行字典排序,该方法会递归调用
+     * @param array1 JSONArray
+     * @param array2 JSONArray
+     * @throws JSONException
+     */
     private static void equals(JSONArray array1, JSONArray array2) throws JSONException {
         if(array1.toString().equals(array2.toString())){
             equalsStr+=true;
@@ -34,11 +37,11 @@ public class JsonUtils {
             logger.debug("array1={},array2={}",array1.toString(),array2.toString());
             return;
         }
-        List<String> list1 = toList(array1);
-        List<String> list2 = toList(array2);
+        array1 = sort(array1);
+        array2 = sort(array2);
         for(int i=0;i<array1.length();i++){
-            String str1 = list1.get(i);
-            String str2 = list2.get(i);
+            String str1 = array1.getString(i);
+            String str2 = array2.getString(i);
             if(str1.startsWith("[")){
                 if(str2.startsWith("[")){
                     equals(new JSONArray(str1),new JSONArray(str2));
@@ -67,14 +70,35 @@ public class JsonUtils {
         }
 
     }
+
+    /**
+     * 将JSONArray转换为list集合，并按照字符串字典进行排序。
+     * @param array 一个JSONArray
+     * @return 按照字典排序后的ArrayList
+     * @throws JSONException
+     */
     private static List<String> toList(JSONArray array) throws JSONException {
         List<String> list = new ArrayList<String>();
         for(int i=0;i<array.length();i++){
-            list.add(array.getString(i));
+            String str = array.getString(i);
+            if(str.startsWith("[")){
+                list.add(array.getJSONArray(i).toString());
+            }else if(str.startsWith("{")){
+                list.add(array.getJSONObject(i).toString());
+            }else{
+                list.add(array.getString(i));
+            }
         }
         Collections.sort(list);
         return list;
     }
+
+    /**
+     * 判断两个JSONObject是否相等,该方法会递归调用
+     * @param json1 JSONObject
+     * @param json2 JSONObject
+     * @throws JSONException
+     */
     private static void equals(JSONObject json1, JSONObject json2) throws JSONException {
         if (json1.length() != json2.length()) {
             equalsStr+=false;
@@ -129,9 +153,18 @@ public class JsonUtils {
             }
         }
     }
+
+    /**
+     * 初始化equalsStr的值
+     */
     private static void init(){
         equalsStr="";
     }
+
+    /**
+     * 得到比较结果，true为相等，false为不相等
+     * @return
+     */
     private static boolean getJsonEqualsResult(){
         logger.info("equalsStr="+equalsStr);
         if(equalsStr.equals("")){
@@ -143,18 +176,40 @@ public class JsonUtils {
             return index==-1;
         }
     }
+
+    /**
+     * 对外提供的判断两个JSONObject是否想等的方法,如果json中包含数组,数组则按字典排序进行排序比较
+     * @param json1 第一个JSONObject
+     * @param json2 第一个JSONObject
+     * @return 返回true则为相等，false为不相等
+     * @throws JSONException
+     */
     public static boolean equalsJson(JSONObject json1,JSONObject json2) throws JSONException {
         init();
         logger.info("需要比较的两个JSONObject为：json1={},json2={}",json1.toString(),json2.toString());
         equals(json1,json2);
         return getJsonEqualsResult();
     }
+
+    /**
+     * 对外提供的判断两个JSONArray是否想等的方法,数组则按字典排序进行排序比较
+     * @param json1 第一个JSONArray
+     * @param json2 第一个JSONArray
+     * @return 返回true则为相等，false为不相等
+     * @throws JSONException
+     */
     public static boolean equalsJsonArray(JSONArray json1,JSONArray json2) throws JSONException {
         init();
         logger.info("需要比较的两个JSONArray为：array1={},array2={}",json1.toString(),json2.toString());
         equals(json1,json2);
         return getJsonEqualsResult();
     }
+
+    /**
+     * 去除JSONArray中key为null的key
+     * @param array JSONArray
+     * @throws JSONException
+     */
     public static void delKeyByValueIsNull(JSONArray array) throws JSONException {
         for(int i=0;i<array.length();i++){
             String str = array.getString(i);
@@ -165,6 +220,12 @@ public class JsonUtils {
             }
         }
     }
+
+    /**
+     * 去除JSONObject中key为null的key
+     * @param json JSONObject
+     * @throws JSONException
+     */
     public static void delKeyByValueIsNull(JSONObject json) throws JSONException {
         Iterator<String> it = json.keys();
         while(it.hasNext()){
@@ -182,6 +243,95 @@ public class JsonUtils {
             }
         }
     }
+
+    /**
+     * 对JSONArray进行字典排序,对包含JSONObject则对key进行字典排序，递归调用
+     * @param json JSONArray
+     * @return 排序后的JSONArray
+     * @throws JSONException
+     */
+    private static JSONArray sort(JSONArray json) throws JSONException {
+        JSONArray sortJson = new JSONArray();
+        List<Object> list = new ArrayList<Object>();
+        for(int i=0;i<json.length();i++){
+            String str = json.getString(i);
+            if(str.startsWith("[")){
+                JSONArray array = sort(json.getJSONArray(i));
+                list.add(array);
+            }else if(str.startsWith("{")){
+                JSONObject jsonObject = sort(json.getJSONObject(i));
+                list.add(jsonObject);
+            }else{
+                list.add(json.getString(i));
+            }
+        }
+        Collections.sort(list, new Comparator<Object>() {
+            public int compare(Object o1, Object o2) {
+                return o1.toString().compareTo(o2.toString());
+            }
+        });
+        for(Object obj:list){
+            if(obj instanceof JSONArray){
+                sortJson.put((JSONArray)obj);
+            }else if(obj instanceof JSONObject){
+                sortJson.put((JSONObject)obj);
+            }else{
+                sortJson.put(obj.toString());
+            }
+        }
+        System.out.println(sortJson.toString());
+        return sortJson;
+    }
+
+    /**
+     * 对JSONObject中的key进行字典排序，递归调用
+     * @param json JSONObject
+     * @return 排序后的JSONObject
+     * @throws JSONException
+     */
+    private static JSONObject sort(JSONObject json) throws JSONException {
+        JSONObject sortJson = new JSONObject(new LinkedHashMap());
+        Iterator<String> it = json.keys();
+        List<String> keys = new ArrayList<String>();
+        while(it.hasNext()){
+            String key = it.next();
+            keys.add(key);
+        }
+        Collections.sort(keys);
+        for(String key:keys){
+            String value = json.getString(key);
+            if(value.startsWith("[")){
+                JSONArray array = sort(json.getJSONArray(key));
+                sortJson.put(key,array);
+            }else if(value.startsWith("{")){
+                JSONObject jsonObject = sort(json.getJSONObject(key));
+                sortJson.put(key,jsonObject);
+            }else{
+                sortJson.put(key,json.getString(key));
+            }
+
+        }
+        return sortJson;
+    }
+
+    /**
+     * 对外提供的对json进行排序的方法
+     * @param json 符合json规范的字符串
+     * @return 排序后的json字符串
+     * @throws JSONException
+     */
+    public static String sort(String json) throws JSONException {
+        String str = null;
+        if(json.startsWith("{")){
+            str = sort(new JSONObject(json)).toString();
+        }else if(json.startsWith("[")){
+            str = sort(new JSONArray(json)).toString();
+        }else{
+            logger.info("传入的json格式错误");
+        }
+        return str;
+    }
+
     public static void main(String[] args) throws JSONException {
         JSONArray j1 = new JSONArray("[[1,2,3],[4,3],[6]]");
         JSONArray j2 = new JSONArray("[[1,2,3],[3,4],[6]]");
